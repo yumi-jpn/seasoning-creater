9d9691e2c39568d2a5960e4d5c7267620defba42
yumi-jpn <yumiruhosi1@gmail.com>
2021-02-09 15:13:11 +0900

    ActiveHashの導入・作成

---
+++ app/models/genre.rb
@@ -0,0 +1,14 @@
+class Price < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '食材の下味' },
+    { id: 3, name: '炒め物' },
+    { id: 4, name: '煮込み物' },
+    { id: 5, name: 'スープの味付け' }
+    { id: 6, name: 'その他(漬物など)' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :tastes
+
+end
--- app/models/taste.rb
+++ app/models/taste.rb
@@ -1,2 +1,4 @@
 class Taste < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
 end
