c47c18ce4880c4a10f8409e73d925b63dffe1a21
yumi-jpn <yumiruhosi1@gmail.com>
2021-02-14 22:34:12 +0900

    テイスト削除機能実装

--- app/controllers/tastes_controller.rb
+++ app/controllers/tastes_controller.rb
@@ -1,6 +1,6 @@
 class TastesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :find_params, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
   before_action :ensure_user, only: [:edit, :update]
 
   def index
@@ -29,27 +29,30 @@ class TastesController < ApplicationController
   end
 
   def update
-   if @taste.update(taste_params)
+    if @taste.update(taste_params)
       redirect_to root_path
-   else
+    else
       render action: :edit
-   end
+    end
+  end
+
+  def destroy
+    @taste.destroy if @taste.user_id == current_user.id
   end
 
   private
 
-   def find_params
-     @taste = Taste.find(params[:id])
-   end
+  def find_params
+    @taste = Taste.find(params[:id])
+  end
 
   def taste_params
     params.require(:taste).permit(:title, :example, :image, :recipe, :genre_id,
                                   taste_seasonings_attributes: [:id, :seasoning_name, :quantity, :_destroy]).merge(user_id: current_user.id)
   end
 
-   def ensure_user
-     @tastes = @taste.user_id == current_user.id
-     redirect_to root_path unless @tastes
-   end
-
+  def ensure_user
+    @tastes = @taste.user_id == current_user.id
+    redirect_to root_path unless @tastes
+  end
 end
