b2054adf49b68f48f06bac8d6da6a145522ca43a
yumi-jpn <yumiruhosi1@gmail.com>
2021-02-15 18:08:42 +0900

    ジャンル別遷移機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :tastes
+  # has_many :comments
+
   validates :nickname, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-
-  # Association
-  has_many :taste
-  # has_many :comments
 end
