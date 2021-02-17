21e6908314b8c263fa159c1db27173f225520693
yumi-jpn <yumiruhosi1@gmail.com>
2021-02-18 00:14:40 +0900

    Nginxの権限 修正1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server ' 35.72.196.190 ', user: 'ec2-user', roles: %w{app db web}
+server '35.72.196.190', user: 'ec2-user', roles: %w{app db web}
