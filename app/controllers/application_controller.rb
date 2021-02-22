6fd373323837a24e2baddd2e81f3777956738bc2
yumi-jpn <yumiruhosi1@gmail.com>
2021-02-22 18:19:22 +0900

    誤字修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
 
-  Basic認証の設定
+  #Basic認証の設定
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
