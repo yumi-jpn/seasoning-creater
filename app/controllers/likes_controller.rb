30fc09d8f11fa54a23249c010fb8cea8bdd3681e
yumi-jpn <yumiruhosi1@gmail.com>
2021-02-28 19:12:25 +0900

    いいね非同期実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,17 @@
 class LikesController < ApplicationController
+  before_action :taste_params
 
   def create
     Like.create(user_id: current_user.id, taste_id: params[:id])
-    redirect_to root_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, taste_id: params[:id]).destroy
-    redirect_to root_path
   end
 
+  private
+
+   def taste_params
+    @taste = Taste.find(params[:id])
+   end
 end
