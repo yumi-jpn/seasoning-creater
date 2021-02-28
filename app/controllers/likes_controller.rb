52560c81246d7c0f85249e732e3b6169bf9044e3
yumi-jpn <yumiruhosi1@gmail.com>
2021-02-28 10:22:12 +0900

    いいね同期実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,19 +1,13 @@
 class LikesController < ApplicationController
-  before_action :taste_params
 
-  # いいねの実行
   def create
     Like.create(user_id: current_user.id, taste_id: params[:id])
+    redirect_to root_path
   end
 
-  # いいねのリセット
   def destroy
     Like.find_by(user_id: current_user.id, taste_id: params[:id]).destroy
+    redirect_to root_path
   end
 
-  private
-
-  def taste_params
-    @taste = Taste.find(params[:id])
-  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,4 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :taste
-
-  with_options presence: true do
-    validates :user_id
-    validates :taste_id
-  end
 end
