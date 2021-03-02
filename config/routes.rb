aff499b7c9596d77a284ca52da965f20a61e0a6c
yumi-jpn <yumiruhosi1@gmail.com>
2021-03-02 11:41:38 +0900

    マイページ遷移のボタン表示

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :tastes do
    resources :comments, only: :create
   end
+  resources :users, only: :show
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
 end
